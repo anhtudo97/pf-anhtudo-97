5ee772bdcefb8a105ef73a36c95336f56b136f58
Zhao Yang <anhtudo97@gmail.com>
2022-01-19 23:00:08 +0700

    feat(configuration): stitches

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const env = process.env.NODE_ENV;
+const inDev = env === "development";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="stylesheet"
+          />
+          {!inDev && (
+            <script
+              async
+              defer
+              data-website-id="b548cc69-60e7-4439-8ede-a2fcd3338ff0"
+              src="https://google-analytics-sucks.herokuapp.com/umami.js"
+            ></script>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyApp
+export default MyDocument;
